da7aef5ee51f44959d383ef2e5fdeb8ea3b3b9a5
Christina Gislason <christinamkcho@gmail.com>
2018-10-11 11:41:38 -0700

    refactored code; no errors now

--- Challenges/Challenge03-BinarySearchArray/Challenge03-BinarySearchArray/Program.cs
+++ Challenges/Challenge03-BinarySearchArray/Challenge03-BinarySearchArray/Program.cs
@@ -3,44 +3,43 @@
 namespace Challenge03_BinarySearchArray
 {
     class Program
-    {
-        //call the method to search the array for search key
-        BinarySearch(); 
-
+    {      
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int[] numArray = { 4, 8, 15, 16, 23, 42 };
-            int invalidResult = -1;
-            int searchKey = 15;
+            int[] testArray = { 4, 8, 15, 16, 23, 42 };
+            int testInvalidResult = -1;
+            int testSearchKey = 15;
+
+            //call the method to search the array for search key
+            Console.WriteLine(BinarySearch(testArray, testSearchKey, testInvalidResult));
         }
 
-        public static int BinarySearch(int[] numArray, int searchKey, int start, int end)
+        public static int BinarySearch(int[] numArray, int searchKey, int invalidResult)
         {
             //declare variables for index positions of array
             int start = 0;
-            int end = 5;
-            int middle = (numArray.Length - 1) / 2;
+            int end = (numArray.Length) - 1;
+            int middle; 
 
             //return index of the array's element that is equal to the search key
             while (start <= end)
             {
+                middle = (start + end) / 2;
+
                 if (numArray[middle] == searchKey)
                 {
                     return middle;
                 }
                 else if (numArray[middle] < searchKey)
                 {
-                    start = middle + 1;
-                    return start;
+                    start = middle + 1;          
                 }
                 else
                 {
-                    end = middle - 1;
-                    return end;
+                    end = middle - 1;                  
                 }
-                
             }
+
             //return -1 if the element does not exist in the array
             return invalidResult;
         }       
