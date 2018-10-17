8c769facb263cdf51dd9cdde9307de284097331e
Christina Gislason <christinamkcho@gmail.com>
2018-10-17 11:30:30 -0700

    Add additional node to list test passing

--- Data-Structures/Lab05-LinkedList/XUnitTestProject1/UnitTest1.cs
+++ Data-Structures/Lab05-LinkedList/XUnitTestProject1/UnitTest1.cs
@@ -15,7 +15,18 @@ namespace XUnitTestProject1
             Assert.Equal(10, linkedList.Head.Value);
         }
         
-        //testing to add another node
+        [Fact]
+        public void TestAddingAdditionalNode()
+        {
+            Node node1 = new Node(10);
+            LinkedList linkedList1 = new LinkedList(new Node(5));
+            linkedList1.Append(node1);
+            Assert.Equal(10, linkedList1.Current.Next.Value);
+        }
+
+
+        
+
 
 
 
