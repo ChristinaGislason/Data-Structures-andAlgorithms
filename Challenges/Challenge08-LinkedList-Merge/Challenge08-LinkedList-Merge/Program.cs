605a9c2f4648da5c3df03f6f49a524d8a5be6f9d
Christina Gislason <christinamkcho@gmail.com>
2018-10-18 10:33:06 -0700

    initial setup

---
+++ Challenges/Challenge08-LinkedList-Merge/Challenge08-LinkedList-Merge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Challenge08_LinkedList_Merge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
