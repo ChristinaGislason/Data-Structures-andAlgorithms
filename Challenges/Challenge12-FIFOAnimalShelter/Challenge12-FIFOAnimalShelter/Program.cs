60a7fa68ca23b47314e3737e8e9cc714a235e8c2
Christina Gislason <christinamkcho@gmail.com>
2018-10-23 12:49:08 -0700

    initial setup

---
+++ Challenges/Challenge12-FIFOAnimalShelter/Challenge12-FIFOAnimalShelter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Challenge12_FIFOAnimalShelter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
