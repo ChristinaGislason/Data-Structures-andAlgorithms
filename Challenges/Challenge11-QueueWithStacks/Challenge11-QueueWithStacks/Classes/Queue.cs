edc88d4c420332f641c5e94d9c9435d7a727a020
Christina Gislason <christinamkcho@gmail.com>
2018-10-23 12:09:18 -0700

    functionality working

---
+++ Challenges/Challenge11-QueueWithStacks/Challenge11-QueueWithStacks/Classes/Queue.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenge11_QueueWithStacks.Classes
+{
+    public class Queue
+    {
+        Stack stack1 = new Stack(new Node(null));
+        Stack stack2 = new Stack(new Node(null));
+        public Node Front { get; set; }
+        public Node Rear { get; set; }
+
+        public Queue(Node node)
+        {
+            stack1.Pop();
+            stack2.Pop();
+            Front = node;
+            Rear = node;
+            stack1.Push(node);
+        }
+
+        // Enqueue
+        public void Enqueue(Node node)
+        {
+            stack1.Push(node);
+            Rear = node;
+        }
+        // Peek
+        public Node Peek()
+        {
+            Node temp = new Node(null);
+            temp = Front;
+            temp.Next = null;
+            return temp;
+        }
+        // Dequeue
+        public Node Dequeue()
+        {
+            if (stack1.Size == 0)
+                throw new Exception("Queue is Empty");
+            Node temp = new Node(null);
+            //Pop all existing elements from stack1, push them onto stack2
+            int stack1Count = stack1.Size;
+            for (int i = 0; i < stack1Count; i++)
+            {
+                stack2.Push(stack1.Pop());
+            }
+            // Save head of our queue
+            temp = stack2.Pop();
+
+            int stack2Count = stack2.Size;
+            //Pop all elements from stack2 and push them back onto stack1
+            for (int i = 0; i < stack2Count; i++)
+            {
+                stack1.Push(stack2.Pop());
+            }
+            return temp;
+        }
+    }
+    
+}
---
+++ Challenges/Challenge11-QueueWithStacks/Challenge11-QueueWithStacks/Classes/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenge11_QueueWithStacks.Classes
+{
+    public class Stack
+    {
+        public Node Top { get; set; }
+        public int Size { get; set; } = 1;
+        public Stack(Node node)
+        {
+            Top = node;
+        }
+
+        //Peek
+        public Node Peek()
+        {
+            // this is a great place for a try/catch (if you want)
+            return Top;
+        }
+
+        //Pop
+        public Node Pop()
+        {
+            Node temp = Peek();
+            Top = Top.Next;
+            temp.Next = null;
+            Size -= 1;
+            return temp;
+        }
+        
+        //Push
+        public void Push(Node node)
+        {
+            node.Next = Top;
+            Top = node;
+            Size += 1;
+        }
+
+        //Define Size
+    }
+}   
