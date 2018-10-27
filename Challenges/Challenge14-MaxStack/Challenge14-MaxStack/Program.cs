f18e49400aff04e8afe05e64a722f838d0572b46
Christina Gislason <christinamkcho@gmail.com>
2018-10-27 10:28:32 -0700

    initial setup

---
+++ Challenges/Challenge14-MaxStack/Challenge14-MaxStack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Challenge14_MaxStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
