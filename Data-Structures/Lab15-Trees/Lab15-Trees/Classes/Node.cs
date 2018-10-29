61cd69a7afb0a7fa45f1119eecbba0bf71596ecc
Christina Gislason <christinamkcho@gmail.com>
2018-10-29 10:55:37 -0700

    created node class

---
+++ Data-Structures/Lab15-Trees/Lab15-Trees/Classes/Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab15_Trees.Classes
+{
+    class Node
+    {
+        /// <summary>
+        /// pointer to the left child node
+        /// </summary>
+        public Node leftChild { get; set; }
+
+        /// <summary>
+        /// pointer to the right child node
+        /// </summary>
+        public Node rightChild { get; set; }
+
+        /// <summary>
+        /// value contained in the node
+        /// </summary> 
+        public int Value { get; set; }
+
+        /// <summary>
+        /// node constructor
+        /// </summary>
+        /// <param name="value"></param>
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ Data-Structures/Lab15-Trees/Lab15-Trees/Classes/Tree.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab15_Trees.Classes
+{
+    class Tree
+    {
+    }
+}
