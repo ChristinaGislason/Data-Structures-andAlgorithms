432589fdaff1536dab6c8c3a563977ffb471c116
Christina Gislason <christinamkcho@gmail.com>
2018-11-03 13:58:07 -0700

    fizzbuzztree working

---
+++ Challenges/Challenge16-FizzBuzzTree/Challenge16-FizzBuzzTree/Classes/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenge16_FizzBuzzTree.Classes
+{
+    class Node
+    {
+        /// <summary>
+        /// pointer to the left child node
+        /// </summary>
+        public Node leftChild { get; set; }
+
+        /// <summary>
+        /// pointer to the right child node
+        /// </summary>
+        public Node rightChild { get; set; }
+
+        /// <summary>
+        /// value contained in the node
+        /// </summary> 
+        public object Value { get; set; }
+
+        /// <summary>
+        /// node constructor
+        /// </summary>
+        /// <param name="value"></param>
+        public Node(int value, Node left = null, Node right = null)
+        {
+            Value = value;
+            leftChild = left;
+            rightChild = right;
+        }
+
+    }
+}
--- Challenges/Challenge16-FizzBuzzTree/Challenge16-FizzBuzzTree/Program.cs
+++ Challenges/Challenge16-FizzBuzzTree/Challenge16-FizzBuzzTree/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Challenge16_FizzBuzzTree.Classes;
+using System;
+using System.Collections.Generic;
 
 namespace Challenge16_FizzBuzzTree
 {
@@ -6,7 +8,22 @@ namespace Challenge16_FizzBuzzTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // instantiate nodes          
+            Node nodeD = new Node(2);
+            Node nodeC = new Node(15);
+            Node nodeB = new Node(5, nodeD);
+            Node nodeA = new Node(3, nodeB, nodeC);
+
+            // add root node to FizzBuzz tree
+            FizzBuzzTree fizzbuzzTree = new FizzBuzzTree(nodeA);
+
+            // write InOrder list of node values to console
+            Console.WriteLine("InOrder list of FizzBuzz Tree:");
+            List<Node> fizzBuzzList = fizzbuzzTree.FizzBuzz();
+            foreach (Node node in fizzBuzzList)
+            {
+                Console.Write($"{node.Value} ");
+            }          
         }
     }
 }
