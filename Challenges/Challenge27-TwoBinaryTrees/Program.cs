82fb824c84f7d5b28451d559ed7cbe12e56571fd
Christina Gislason <christinamkcho@gmail.com>
2018-11-14 23:53:59 -0800

    initial setup

---
+++ Challenges/Challenge27-TwoBinaryTrees/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Challenge27_TwoBinaryTrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
