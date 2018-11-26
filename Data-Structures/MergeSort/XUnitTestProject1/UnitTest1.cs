1a3074811c4fcd0450e7dcaf17feb31ae2dd4a64
Christina Gislason <christinamkcho@gmail.com>
2018-11-25 17:33:03 -0800

    Test for MergeSort method for large array passing

--- Data-Structures/MergeSort/XUnitTestProject1/UnitTest1.cs
+++ Data-Structures/MergeSort/XUnitTestProject1/UnitTest1.cs
@@ -10,10 +10,34 @@ namespace XUnitTestProject1
         /// Test MergeSort method to return sorted array
         /// </summary>
         [Fact]
-        public void TestMergeSort()
+        public void TestMergeSortForArray()
         {
             int[] arr = { 3, 2, 1 };
             int[] expectedArr = { 1, 2, 3 };
+            Program.MergeSort(arr);
+            Assert.Equal(expectedArr, arr);
+        }
+
+        /// <summary>
+        /// Test MergeSort method for array of size 1
+        /// </summary>
+        [Fact]
+        public void TestMergeSortForArraySizeOne()
+        {
+            int[] arr = { 3 };
+            int[] expectedArr = { 3 };
+            Program.MergeSort(arr);
+            Assert.Equal(expectedArr, arr);
+        }
+
+        /// <summary>
+        /// Test MergeSort method for large array
+        /// </summary>
+        [Fact]
+        public void TestMergeSortForLargeArray()
+        {
+            int[] arr = { 365, 555, 10, 252, 1001, 7 };
+            int[] expectedArr = { 7, 10, 252, 365, 555, 1001 };
             Program.MergeSort(arr);
             Assert.Equal(expectedArr, arr);
         }
