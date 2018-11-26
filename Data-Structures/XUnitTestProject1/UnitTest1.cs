c39d31cff22ddd3ff11b21696dd3acdb60c291ca
Christina Gislason <christinamkcho@gmail.com>
2018-11-25 19:23:22 -0800

    testing quicksort method passes

--- Data-Structures/XUnitTestProject1/UnitTest1.cs
+++ Data-Structures/XUnitTestProject1/UnitTest1.cs
@@ -1,3 +1,4 @@
+using QuickSort;
 using System;
 using Xunit;
 
@@ -5,10 +6,16 @@ namespace XUnitTestProject1
 {
     public class UnitTest1
     {
+        /// <summary>
+        /// Test QuickSort method to return sorted array
+        /// </summary>
         [Fact]
-        public void Test1()
+        public void TestQuickSortForArray()
         {
-
+            int[] arr = { 3, 2, 1 };
+            int[] expectedArr = { 1, 2, 3 };
+            Program.QuickSort(arr, 0, arr.Length -1);
+            Assert.Equal(expectedArr, arr);
         }
     }
 }
